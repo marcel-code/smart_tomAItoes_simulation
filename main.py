58c7e49b83a46f80faa48ed53089d9aa1d577b31
jonasschira <jonas.schira@gmail.com>
2024-04-08 23:31:13 +0200

    first test requesting the simulator

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
-from src.utils.testcode import add_nums
+import requests
+import json
 
-a = 10
-b = 10
-print("{}+{}={}".format(
-    a,
-    b,
-    add_nums(a,b)
-))
+## MAIN SECTION
+
+if __name__ == "__main__":
+
+    # Test: passing control parameters to the model
+
+    # select key
+    key_A = 'SmartToms-A-gl4d-1cam' # key to simulator A
+    key_B = 'SmartToms-B-gl4d-1cam' # key to simulator B
+    key = key_A
+
+    # read input json
+    param_path = 'example_input.json'
+    with open(param_path, 'r') as file:
+        param = file.read()
+
+    # simulator
+    url = 'https://www.digigreenhouse.wur.nl/AGC2024/model/kaspro'
+    data = {'key': key, 'parameters': json.loads(param)}
+    response = requests.post(url, json=data) # https://realpython.com/python-requests/#the-response
+
+    # print out the response
+    print("Response:", response)
+    print("Status Code:", response.status_code)
+    print("Response Text:", response.text)
+    print("Response Content:", response.content)
+    print("Response Reason:", response.reason)
+    print("Response json:", response.json())
+    
+    # Write the response json to a file
+    with open('response.json', 'w') as f:
+        json.dump(response.json(), f)
